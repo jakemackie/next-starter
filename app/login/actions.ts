e3af6aad75314d564bf51dfb155fea31c26b97d4
jakemackie <jake.wmackie@gmail.com>
2024-11-28 20:20:05 +0000

    feat: Lotta frontend changes

--- app/login/actions.ts
+++ app/login/actions.ts
@@ -2,7 +2,6 @@
 
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
-
 import { createClient } from "@/utils/supabase/server";
 
 export async function login(formData: FormData) {
@@ -24,7 +23,7 @@ export async function login(formData: FormData) {
   }
 
   revalidatePath("/", "layout");
-  redirect("/");
+  redirect("/private");
 }
 
 export async function signup(formData: FormData) {
--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,23 @@
-import prisma from "@/lib/db";
+import { Spotlight } from "@/components/ui/spotlight";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
 
 export default async function Home() {
-  const test = await prisma.test.findFirstOrThrow({
-    where: {
-      id: 1,
-    },
-  });
   return (
-    <div>
-      Test {test.id.toString()} was created at{" "}
-      {test.created_at.toLocaleTimeString()}
+    <div className="min-h-screen w-full rounded-md flex md:items-center md:justify-center antialiased bg-grid-white/[0.02] relative overflow-hidden">
+      <Spotlight className="-top-40 left-0 md:left-60" fill="white" />
+      <div className="flex flex-col items-center p-4 max-w-7xl mx-auto relative z-10 w-full pt-40 md:pt-0">
+        <h1 className="text-4xl md:text-7xl font-bold text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50">
+          Next starter <br /> template.
+        </h1>
+        <p className="mt-4 font-normal text-base text-neutral-300 max-w-lg text-center mx-auto">
+          A starter template that lets you build scalable modern web
+          applications that look damn good.
+        </p>
+        <Link href="/login" className="mt-4">
+          <Button variant="outline">Login</Button>
+        </Link>
+      </div>
     </div>
   );
 }
