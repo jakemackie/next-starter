1ec2f9a3faf9ba40a0c892c5f301ad50a40f7ec0
jakemackie <jake.wmackie@gmail.com>
2024-12-12 20:56:21 +0000

    feat: Capturing events with Posthog

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { CSPostHogProvider } from "./providers";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -19,7 +20,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={`${inter.className} antialiased dark`}>{children}</body>
+      <CSPostHogProvider>
+        <body className={`${inter.className} antialiased dark`}>
+          {children}
+        </body>
+      </CSPostHogProvider>
     </html>
   );
 }
