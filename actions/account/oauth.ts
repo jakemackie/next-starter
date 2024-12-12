dac356b6f3427b619aab8ae9a4664e89a053be8f
jakemackie <jake.wmackie@gmail.com>
2024-12-12 18:12:37 +0000

    feat: GitHub oauth

---
+++ actions/account/oauth.ts
@@ -0,0 +1,22 @@
+"use server";
+
+import { createClient } from "@/utils/supabase/server";
+import { Provider } from "@supabase/supabase-js";
+import { redirect } from "next/navigation";
+
+export async function oauth(provider: Provider) {
+  const supabase = await createClient();
+
+  const callbackUrl = new URL("/auth/callback", process.env.NEXT_PUBLIC_URL);
+
+  const { data, error } = await supabase.auth.signInWithOAuth({
+    provider,
+    options: {
+      redirectTo: callbackUrl.toString(),
+    },
+  });
+
+  if (error) throw new Error(error.message);
+
+  redirect(data.url);
+}
--- app/(main)/login/page.tsx
+++ app/(main)/login/page.tsx
@@ -2,7 +2,7 @@ import { LoginForm } from "@/components/shared/loginForm";
 
 export default function LoginPage() {
   return (
-    <div className="flex items-center justify-center h-screen">
+    <div className="mx-auto flex flex-col items-center justify-center h-screen">
       <LoginForm />
     </div>
   );
