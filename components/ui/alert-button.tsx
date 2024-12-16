5bda05295ebc09fd7578488d8771c6ceee745fa9
Jake Mackie <jake@hiyield.co.uk>
2024-12-16 07:43:55 +0000

    amend: Alert button for light mode

--- components/ui/alert-button.tsx
+++ components/ui/alert-button.tsx
@@ -7,11 +7,11 @@ interface ButtonProps {
 
 const AlertButton: React.FC<ButtonProps> = ({ children }) => {
   return (
-    <button className="bg-slate-800 no-underline group cursor-pointer relative shadow-2xl shadow-zinc-900 rounded-full p-px text-xs font-semibold leading-6 text-white inline-block">
+    <button className="bg-slate-800 no-underline group cursor-pointer relative shadow-2xl shadow-input rounded-full p-px text-xs font-semibold leading-6 text-foreground inline-block">
       <span className="absolute inset-0 overflow-hidden rounded-full">
         <span className="absolute inset-0 rounded-full bg-[image:radial-gradient(75%_100%_at_50%_0%,rgba(56,189,248,0.6)_0%,rgba(56,189,248,0)_75%)] opacity-0 transition-opacity duration-500 group-hover:opacity-100" />
       </span>
-      <div className="relative flex space-x-2 items-center z-10 rounded-full bg-zinc-950 py-0.5 px-4 ring-1 ring-white/10">
+      <div className="relative flex space-x-2 items-center z-10 rounded-full bg-background py-0.5 px-4 ring-1 ring-white/10">
         <span>{children}</span>
         <ChevronRight className="size-2.5" />
       </div>
